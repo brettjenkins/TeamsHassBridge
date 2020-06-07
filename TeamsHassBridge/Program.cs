6690e067eb4c8ee53005950e9eed55024c18c039
Brett Jenkins <brett@brettjenkins.co.uk>
2020-06-07 01:50:38 +0100

    Added idle detection, and fixed a bug that made more PUTs than needed

--- TeamsHassBridge/Program.cs
+++ TeamsHassBridge/Program.cs
@@ -12,7 +12,7 @@ namespace TeamsHassBridge
 {
     public class Program
     {
-
+        private const int TeamsIdleTimeSecs = 360;
         private static TeamsStatus LastStatus { get; set; } = new TeamsStatus();
         private static bool Startup = true;
 
@@ -69,7 +69,35 @@ namespace TeamsHassBridge
             else if (line.Contains("Added NewActivity"))            
                 status.TeamsUnread = true;            
             else if (line.Contains("Removing NewActivity"))            
-                status.TeamsUnread = false;            
+                status.TeamsUnread = false;      
+            else if (line.Contains("Machine is unlocked"))
+                status.TeamsIdle = false;
+            else if (line.Contains("Machine is locked"))
+                status.TeamsIdle = true;
+            else if (line.Contains("Machine has been idle for"))
+            {
+                var regex = new Regex("Machine has been idle for ([0-9]*(\\.[0-9]*)?) seconds");
+                var match = regex.Match(line);
+                if (match.Success)
+                {
+                    var secondsIdle = double.Parse(match.Groups[1].Value);
+                    Console.WriteLine($"Idle for {secondsIdle}");
+                    status.TeamsIdle = secondsIdle > TeamsIdleTimeSecs;
+                }
+            }
+                
+            /* The Teams log doesn't report when the status has gone idle, annoyingly, and it's not documented by MS but from observing the teams idle logic seems to be as thus:
+               Note as well I'm saying Idle/Not-Idle because the status returns to the last set status - this is why I set a bool for idle and not change the status,
+               because that status will be restored when not idle
+                * IF UNLOCKED:
+                    * Teams seems to check idle time every few minutes (between 2-5 minutes each check in my testing), so there's a bit of a lag depending on when the check is
+                    * After teams has realised that the PC has been idle for more than 360 seconds (6 minutes), Teams will report the user as Idle
+                    * After teams has realised that the PC has been idle for less than 360 seconds, if idle Teams will report the user as Not-Idle
+                    * (this isn't instant as it depends on the idle check that teams runs every few minutes)
+                * LOCKING the PC will instantly set the status to Idle
+                * UNLOCKING the PC will instantly set the status to Not-Idle
+             
+             */
             
             if (line.Contains("-- info -- Added"))
             {
@@ -105,17 +133,20 @@ namespace TeamsHassBridge
         {
             if (!teamsStatus.IsNull())
             {
+                if (!teamsStatus.TeamsOnCall.HasValue)
+                    teamsStatus.TeamsOnCall = LastStatus.TeamsOnCall;
+
+                if (!teamsStatus.TeamsState.HasValue)
+                    teamsStatus.TeamsState = LastStatus.TeamsState;
+
+                if (!teamsStatus.TeamsUnread.HasValue)
+                    teamsStatus.TeamsUnread = LastStatus.TeamsUnread;
+
+                if (!teamsStatus.TeamsIdle.HasValue)
+                    teamsStatus.TeamsIdle = LastStatus.TeamsIdle;
+
                 if (!LastStatus.Equals(teamsStatus) || force)
                 {
-                    if (teamsStatus.TeamsOnCall == null)
-                        teamsStatus.TeamsOnCall = LastStatus.TeamsOnCall;
-
-                    if (teamsStatus.TeamsState == null)
-                        teamsStatus.TeamsState = LastStatus.TeamsState;
-
-                    if (teamsStatus.TeamsUnread == null)
-                        teamsStatus.TeamsUnread = LastStatus.TeamsUnread;
-
                     using (var client = new HttpClient())
                     {
                         LastStatus = teamsStatus;
--- TeamsHassBridge/TeamsStatus.cs
+++ TeamsHassBridge/TeamsStatus.cs
@@ -5,16 +5,19 @@
         public States? TeamsState { get; set; }
         public bool? TeamsOnCall { get; set; }
         public bool? TeamsUnread { get; set; }
+        public bool? TeamsIdle { get; set; }
 
         public bool Equals(TeamsStatus obj)
         {
             return obj != null && (obj.TeamsState == TeamsState &&
-                                    obj.TeamsOnCall == TeamsOnCall && obj.TeamsUnread == TeamsUnread);
+                                   obj.TeamsOnCall == TeamsOnCall && 
+                                   obj.TeamsUnread == TeamsUnread && 
+                                   obj.TeamsIdle == TeamsIdle);
         }
 
         public bool IsNull()
         {
-            return (this.TeamsOnCall == null && this.TeamsState == null && this.TeamsUnread == null);
+            return (!TeamsOnCall.HasValue && !TeamsState.HasValue && !TeamsUnread.HasValue && !TeamsIdle.HasValue);
         }
     }
 }
